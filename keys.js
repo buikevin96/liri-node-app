51f6360ae9812cc52a96909dbbdb11b19132e58c
buikevin96 <buikevin96@gmail.com>
2017-08-02 11:30:36 -0400

    Added package.json, keys.js, random.txt, liri.js

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<uktQccsXTeLlYh1fB1YGkcHVi>',
+  consumer_secret: '<nqm7RjRGHkQlFPSrPcf8eFnqpgofj3BIYBuZiUfEyQJjWXE4XT>',
+  access_token_key: '<892094572800692224-yg4NkmSuNFBHnmaIWFd26HcLpDu6XwW>',
+  access_token_secret: '<i3837ds2RNzTX5L1rpTbIizz2pyY3TWGV60jgNA62FLdl>',
+}
---
+++ liri.js
@@ -0,0 +1,5 @@
+// Is this where we place these?
+var request = require('request');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-app');
+
